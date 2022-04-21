da4007ba7217230f0f3feaf00ed1bb6f3f314688
YWJWORLD <woojin950104@naver.com>
2022-04-21 19:34:02 +0900

    first..

---
+++ generator.py
@@ -0,0 +1,85 @@
+from transmitter import transmitters
+from gnuradio import channels, gr, blocks
+import numpy as np
+import numpy.fft
+import random
+import time
+import pickle
+
+'''
+Generate dataset with dynamic channel model across range of SNRs
+'''
+
+apply_channel = True
+
+dataset = {}
+
+# The output format looks like this
+# {('mod type', SNR): np.array(nvecs_per_key, 2, vec_length), etc}
+
+nvecs_per_key = 500
+vec_length = 2048
+samp_rate = 2e6
+snr_vals = range(-20,12,2)
+for snr in snr_vals:
+    print("snr is {}".format(snr))
+    for fmcw_type in transmitters.keys():
+        for i,mod_type in enumerate(transmitters[fmcw_type]):
+            dataset[(mod_type.modname, snr)] = np.zeros([nvecs_per_key,2,vec_length], dtype=np.float32)
+            # moar vectors!
+            insufficient_modsnr_vectors = True
+            modvec_indx = 0
+            while insufficient_modsnr_vectors:
+                if fmcw_type == "FMCW_LFM":
+                    src_mod = mod_type(amp=1,offset=0,samp_rate=samp_rate)
+                elif fmcw_type == "FMCW_Barker":
+                    src_mod = mod_type(code_length = 13)
+                elif fmcw_type == "FMCW_Costas":
+                    src_mod = mod_type(prime_number=11, primitive_root=2, samp_rate=samp_rate)
+                elif fmcw_type == "FMCW_Polyphase":
+                    src_mod = mod_type(code_length = 16,kind_of_signal = mod_type.modname)
+                elif fmcw_type == "FMCW_Polytime":
+                    src_mod = mod_type(Kind_of_signal = mod_type.modname,delta_f=samp_rate/20, phase_state = 2, segment = 4)
+                # print(src_mod)
+                fD = 1
+                delays = [0.0, 0.9, 1.7]
+                mags = [1, 0.8, 0.3]
+                ntaps = 8
+                noise_amp = 10**(-snr/10.0)
+                chan = channels.dynamic_channel_model(200e3, 0.01, 50, .01, 0.5e3, 8, fD, True, 4, delays, mags, ntaps, noise_amp, 0x1337)
+                # print("check...1")
+                snk = blocks.vector_sink_c()
+                # print("check...2")
+                tb = gr.top_block()
+                # connect blocks
+                if apply_channel:
+                    tb.connect(src_mod,chan,snk)
+                else:
+                    tb.connect(src_mod,snk)
+                # print("check...3")
+                # tb.start()
+                # time.sleep(0.01)
+                # tb.stop()
+                tb.run()
+                # del tb
+                raw_output_vector = np.array(snk.data(), dtype=np.complex64)
+                # print(raw_output_vector.shape)
+                # start the sampler some random time after channel model transients (arbitray values here)
+                sampler_indx = random.randint(50,500)
+                while sampler_indx + vec_length < len(raw_output_vector) and modvec_indx < nvecs_per_key:
+                    sampled_vector = raw_output_vector[sampler_indx:sampler_indx+vec_length]
+                    energy = np.sum((np.abs(sampled_vector)))
+                    sampled_vector = sampled_vector / energy
+                    dataset[(mod_type.modname, snr)][modvec_indx,0,:] = np.real(sampled_vector)
+                    dataset[(mod_type.modname, snr)][modvec_indx,1,:] = np.imag(sampled_vector)
+                    sampler_indx += random.randint(vec_length, round(len(raw_output_vector)*.5))
+                    modvec_indx += 1
+                if modvec_indx == nvecs_per_key:
+                    # we're all done
+                    insufficient_modsnr_vectors = False
+
+
+print("all done. writing to disk")
+with open('FMCW_RADAR2022_dict_ver2.pkl','wb') as f:
+    pickle.dump(dataset, f)
+
---
+++ transmitter.py
@@ -0,0 +1,285 @@
+import time,math,sys,signal
+
+from scipy import random
+from gnuradio import analog, blocks, gr ,filter
+from gnuradio.filter import firdes
+# from gnuradio.filter import firdes
+import epy_block_0, epy_block_0_0
+import sys
+import signal
+
+class transmitter_LFM(gr.hier_block2):
+    modname = 'LFM'
+    def __init__(self, amp, offset, samp_rate, vco_amplitude=1,limit=10000):
+        gr.hier_block2.__init__(
+            self, "LFM Waveform Block",
+                gr.io_signature(0, 0, 0),
+                gr.io_signature(1, 1, gr.sizeof_gr_complex*1),
+        )
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.amp = amp
+        self.offset = offset
+        self.samp_rate = samp_rate
+        self.vco_amplitude = vco_amplitude
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.hilbert_fc_0 = filter.hilbert_fc(50, firdes.WIN_BLACKMAN_hARRIS, 6.76)
+        self.blocks_vco_f_0 = blocks.vco_f(samp_rate, samp_rate, vco_amplitude)
+        self.skip = blocks.skiphead(gr.sizeof_gr_complex,random.randint(1,1000000))
+        self.limit = blocks.head(gr.sizeof_gr_complex,limit)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_TRI_WAVE, samp_rate/1.3e3, amp, offset, )
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_vco_f_0, 0))
+        self.connect((self.blocks_vco_f_0, 0), (self.hilbert_fc_0, 0))
+        self.connect((self.hilbert_fc_0, 0), (self.skip,0), (self.limit, 0),(self,0))
+        
+
+
+class transmitter_barker(gr.hier_block2):
+    modname = 'Barker'
+    def __init__(self, code_length=13, samp_rate=2e6, source_amplitude=1,limit=10000):
+        gr.hier_block2.__init__(
+            self, "Barker Waveform Block",
+                gr.io_signature(0, 0, 0),
+                gr.io_signature(1, 1, gr.sizeof_gr_complex*1),
+        )
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.code_length = code_length
+        self.samp_rate = samp_rate
+        self.source_amplitude = source_amplitude
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.interp = interp = 10
+        self.samp_freq = samp_freq = samp_rate/interp
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.skip = blocks.skiphead(gr.sizeof_gr_complex,random.randint(1,1000000))
+        self.limit = blocks.head(gr.sizeof_gr_complex,limit)
+        self.epy_block_0_0 = epy_block_0_0.blk(code_length=code_length)
+        self.blocks_vector_source_x_1 = blocks.vector_source_f((0, 0, 0), True, 1, [])
+        self.blocks_repeat_0_0 = blocks.repeat(gr.sizeof_float*1, int(samp_rate/samp_freq))
+        self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, samp_freq, source_amplitude, 0, 0)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.skip,0), (self.limit,0),(self, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.blocks_repeat_0_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.blocks_vector_source_x_1, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.epy_block_0_0, 0), (self.blocks_repeat_0_0, 0))
+
+class transmitter_costas(gr.hier_block2):
+    modname = 'Costas'
+    def __init__(self, prime_number=11, primitive_root=2, samp_rate=0, seq_len=1024, source_amplitude=1,limit=10000):
+        gr.hier_block2.__init__(
+            self, "Costas Waveform Blocks",
+                gr.io_signature(0, 0, 0),
+                gr.io_signature(1, 1, gr.sizeof_gr_complex*1),
+        )
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.prime_number = prime_number
+        self.primitive_root = primitive_root
+        self.samp_rate = samp_rate
+        self.seq_len = seq_len
+        self.source_amplitude = source_amplitude
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.multi_const = multi_const = prime_number-1
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.skip = blocks.skiphead(gr.sizeof_gr_complex,random.randint(1,1000000))
+        self.limit = blocks.head(gr.sizeof_gr_complex,limit)
+        self.epy_block_0 = epy_block_0.blk(primitive_root=primitive_root, prime_number=prime_number)
+        self.blocks_vco_f_0 = blocks.vco_f(samp_rate, samp_rate, source_amplitude)
+        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_float*1, int(seq_len / (prime_number-1)))
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_float_to_complex_0, 0),(self.skip,0), (self.limit,0), (self, 0))
+        self.connect((self.blocks_repeat_0, 0), (self.blocks_vco_f_0, 0))
+        self.connect((self.blocks_vco_f_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.blocks_vco_f_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.epy_block_0, 0), (self.blocks_repeat_0, 0))
+
+class transmitter_polyphase(gr.hier_block2):
+    def __init__(self, code_length=16, kind_of_signal='Frank', samp_freq=2e6/4, samp_rate=2e6, source_amplitude=1,limit=10000):
+        gr.hier_block2.__init__(
+            self, "Polyphase Waveform Block",
+                gr.io_signature(0, 0, 0),
+                gr.io_signature(1, 1, gr.sizeof_gr_complex*1),
+        )
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.code_length = code_length
+        self.kind_of_signal = kind_of_signal
+        self.samp_freq = samp_freq
+        self.samp_rate = samp_rate
+        self.source_amplitude = source_amplitude
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.skip = blocks.skiphead(gr.sizeof_gr_complex,random.randint(1,1000000))
+        self.limit = blocks.head(gr.sizeof_gr_complex,limit)
+        self.epy_block_0_0 = epy_block_0_0.blk(code_length=code_length, kind_of_signal=kind_of_signal)
+        self.blocks_repeat_0_0 = blocks.repeat(gr.sizeof_float*1, int(samp_rate/samp_freq))
+        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_float*1, int(samp_rate/samp_freq))
+        self.blocks_multiply_xx_0_0 = blocks.multiply_vff(1)
+        self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, samp_freq, source_amplitude, 0, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, samp_freq, source_amplitude, 0, 0)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0_0, 1))
+        self.connect((self.blocks_float_to_complex_0, 0),(self.skip,0), (self.limit,0), (self, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.blocks_multiply_xx_0_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.blocks_repeat_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.blocks_repeat_0_0, 0), (self.blocks_multiply_xx_0_0, 0))
+        self.connect((self.epy_block_0_0, 0), (self.blocks_repeat_0, 0))
+        self.connect((self.epy_block_0_0, 1), (self.blocks_repeat_0_0, 0))
+
+class transmitter_frank(transmitter_polyphase):
+    modname = 'Frank'
+    def __init__(self, code_length,limit=10000, kind_of_signal=modname,samp_freq=2e6/4,  samp_rate=2e6,  source_amplitude=1):
+        super().__init__(code_length, kind_of_signal, samp_freq, samp_rate, source_amplitude,limit)
+
+class transmitter_P1(transmitter_polyphase):
+    modname = 'P1'
+    def __init__(self, code_length=16,limit=10000, kind_of_signal=modname, samp_freq=2e6/4, samp_rate=2e6, source_amplitude=1):
+        super().__init__(code_length, kind_of_signal, samp_freq, samp_rate, source_amplitude,limit)
+
+class transmitter_P2(transmitter_polyphase):
+    modname = 'P2'
+    def __init__(self, code_length=16,limit=10000, kind_of_signal=modname, samp_freq=2e6/4, samp_rate=2e6, source_amplitude=1):
+        super().__init__(code_length, kind_of_signal, samp_freq, samp_rate, source_amplitude,limit)
+
+class transmitter_P3(transmitter_polyphase):
+    modname = 'P3'
+    def __init__(self, code_length=16,limit=10000, kind_of_signal=modname, samp_freq=2e6/4, samp_rate=2e6, source_amplitude=1):
+        super().__init__(code_length, kind_of_signal, samp_freq, samp_rate, source_amplitude,limit)
+
+class transmitter_P4(transmitter_polyphase):
+    modname = 'P4'
+    def __init__(self, code_length=16,limit=10000, kind_of_signal=modname, samp_freq=2e6/4, samp_rate=2e6, source_amplitude=1):
+        super().__init__(code_length, kind_of_signal, samp_freq, samp_rate, source_amplitude,limit)
+
+
+class transmitter_polytime(gr.hier_block2):
+    def __init__(self, Kind_of_signal='T1', center_freq=910e6, delta_f=2000, interp=2e6/500e3, phase_state=2, samp_rate=2e6, segment=4, signal_freq=2e6/4, source_amplitude=1,limit=10000):
+        gr.hier_block2.__init__(
+            self, "Polytime waveform Block",
+                gr.io_signature(0, 0, 0),
+                gr.io_signature(1, 1, gr.sizeof_gr_complex*1),
+        )
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.Kind_of_signal = Kind_of_signal
+        self.center_freq = center_freq
+        self.delta_f = delta_f
+        self.interp = interp
+        self.phase_state = phase_state
+        self.samp_rate = samp_rate
+        self.segment = segment
+        self.signal_freq = signal_freq
+        self.source_amplitude = source_amplitude
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.skip = blocks.skiphead(gr.sizeof_gr_complex,random.randint(1,1000000))
+        self.limit = blocks.head(gr.sizeof_gr_complex,limit)
+        self.epy_block_0 = epy_block_0.blk(kind_of_signal=Kind_of_signal, number_of_segment=segment, phase_state=phase_state, delta_f=delta_f, t_seg=0.004, t_interval=0.0001)
+        self.blocks_repeat_0_0 = blocks.repeat(gr.sizeof_float*1, int(interp))
+        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_float*1, int(interp))
+        self.blocks_multiply_xx_0_0 = blocks.multiply_vff(1)
+        self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, signal_freq, source_amplitude, 0, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, signal_freq, source_amplitude, 0, 0)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0_0, 0))
+        
+        self.connect((self.blocks_float_to_complex_0, 0),(self.skip,0), (self.limit,0),(self, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.blocks_multiply_xx_0_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.blocks_repeat_0, 0), (self.blocks_multiply_xx_0_0, 1))
+        self.connect((self.blocks_repeat_0_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.epy_block_0, 1), (self.blocks_repeat_0, 0))
+        self.connect((self.epy_block_0, 0), (self.blocks_repeat_0_0, 0))
+
+class transmitter_T1(transmitter_polytime):
+    modname = 'T1'
+    def __init__(self, Kind_of_signal=modname,limit=10000, center_freq=910e6, delta_f=2000, interp=2e6/500e3, phase_state=2, samp_rate=2e6, segment=4, signal_freq=2e6/4, source_amplitude=1):
+        super().__init__(Kind_of_signal, center_freq, delta_f,interp,phase_state, samp_rate,segment, signal_freq, source_amplitude,limit)
+
+class transmitter_T2(transmitter_polytime):
+    modname = 'T2'
+    def __init__(self, Kind_of_signal=modname,limit=10000, center_freq=910e6, delta_f=2000, interp=2e6/500e3, phase_state=2, samp_rate=2e6, segment=4, signal_freq=2e6/4, source_amplitude=1):
+        super().__init__(Kind_of_signal, center_freq, delta_f,interp,phase_state, samp_rate,segment, signal_freq, source_amplitude,limit)
+
+class transmitter_T3(transmitter_polytime):
+    modname = 'T3'
+    def __init__(self, Kind_of_signal=modname,limit=10000, center_freq=910e6, delta_f=2000, interp=2e6/500e3, phase_state=2, samp_rate=2e6, segment=4, signal_freq=2e6/4, source_amplitude=1):
+        super().__init__(Kind_of_signal, center_freq, delta_f,interp,phase_state, samp_rate,segment, signal_freq, source_amplitude,limit)
+
+class transmitter_T4(transmitter_polytime):
+    modname = 'T4'
+    def __init__(self, Kind_of_signal=modname,limit=10000, center_freq=910e6, delta_f=2000, interp=2e6/500e3, phase_state=2, samp_rate=2e6, segment=4, signal_freq=2e6/4, source_amplitude=1):
+        super().__init__(Kind_of_signal, center_freq, delta_f,interp,phase_state, samp_rate,segment, signal_freq, source_amplitude,limit)
+    
+
+transmitters = {
+    "FMCW_Polyphase":[transmitter_frank,transmitter_P1,transmitter_P2,transmitter_P3,transmitter_P4],
+    "FMCW_LFM":[transmitter_LFM],
+    
+    "FMCW_Polytime":[transmitter_T1,transmitter_T2,transmitter_T3,transmitter_T4],
+    "FMCW_Barker":[transmitter_barker],
+    "FMCW_Costas":[transmitter_costas],
+    
+}
